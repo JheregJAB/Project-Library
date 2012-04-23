6fb1799678cb33409b88eb4484dbeb9c2f38170b
Jacob Burkamper <jacob.burkamper@gmail.com>
2012-04-23 00:55:07 -0500

    Add in books class, addBook Method and dependencies.

--- src/project_library/Book.java
+++ src/project_library/Book.java
@@ -10,20 +10,167 @@ package project_library;
  */
 public class Book 
 {
-    private String string;
-    
+    private boolean isSet;
+    private boolean restrictions;
+    private boolean fiction;
+    private boolean nonFiction;
+    private String title;
+    private String author;
+    private String condition;
+    private String status;
+    private String category;
+    private String description;
+    private String checkOutDate;
+    private String summary;
+    private double price;
+    private int checkedOutBy;
+    private static int bookID;
+   
     Book()
     {
-        string = "This is a book!";
+        isSet = false;
+        restrictions = false;
+        fiction = true;
+        nonFiction = true;
+    }
+   
+    public String getTitle()
+    {
+        return title;
     }
     
-    public String getString()
+    public String getAuthor()
     {
-        return string;
+        return author;
+    }
+  
+    public String getCondition()
+    {
+        return condition;
+    }
+  
+    public String getStatus()
+    {
+        return status;
+    }
+  
+    public String getCategory()
+    {
+        return category;
+    }
+  
+    public String getDescription()
+    {
+        return description;
+    }
+   
+    public String getCheckOutDate()
+    {
+        return checkOutDate;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+ 
+    public int getCheckedOutBy()
+    {
+        return checkedOutBy;
+    }
+  
+    public static int getBookID()
+    {
+        return bookID;
     }
     
-    public void setString(String string)
+    public String getSummary()
     {
-        this.string = string;
+        return summary;
+    }
+    
+    public void setSummary(String newSummary)
+    {
+        summary = newSummary;
+    }
+  
+    public void setTitle(String newValue)
+    {
+        title = newValue;
+    }
+  
+    public void setCondition(String newValue)
+    {
+        condition = newValue;
+    }
+  
+    public void setStatus(String newValue)
+    {
+        status = newValue;
+    }
+  
+    public void setCategory(String newValue)
+    {
+        category = newValue;
+    }
+  
+    public void setDescription(String newValue)
+    {
+        description = newValue;
+    }
+  
+    public void setCheckOutDate(String newValue)
+    {
+        checkOutDate = newValue;
+    }
+
+    public void setPrice(double newValue)
+    {
+        price = newValue;
+    }
+
+    public void setCheckedOutBy(int newValue)
+    {
+        checkedOutBy = newValue;
+    }
+   
+    public void setBookID(int newValue)
+    {
+        bookID = newValue;
+    }
+    
+    public void setAuthor(String newAuthor)
+    {
+        author = newAuthor;
+    }
+   
+    public boolean isSet()
+    {
+        return isSet;
+    }
+   
+    public void setIsSet()
+    {
+        isSet = true;
+    }
+    
+    public boolean Restricted()
+    {
+        return restrictions;
+    }
+    
+    public boolean Fiction()
+    {
+        return fiction;
+    }
+    
+    public void setFiction(boolean newValue)
+    {
+        fiction = newValue;
+    }
+    
+    public void setRestricted(boolean isRestricted)
+    {
+        restrictions = isRestricted;
     }
 }
